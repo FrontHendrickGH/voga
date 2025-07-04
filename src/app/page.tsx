6e9032fd5dc3ca9fce83925d81968eb5bc739ee4
Hendrick <hendrick@nebulosastudio.com>
2025-07-04 17:40:25 -0600

    added footer to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Footer from "@/components/Footer";
 import styles from "./page.module.css";
+import Banner from "@/components/Banner";
 
 export default function Home() {
-	return <div className={styles.page}></div>;
+	return (
+		<div className={styles.page}>
+			<Banner
+				image={{ src: "/banner-nosotros.png", alt: "sobre nosotros" }}
+				title={"Quiénes Somos"}
+			/>
+			<Footer
+				paddingTop={271}
+				title="Haz Realidad el Evento de Tus Sueños."
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+				button={{ title: "Contáctanos", href: "/contacto" }}
+			/>
+		</div>
+	);
 }
