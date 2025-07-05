d88b2ab034b58c9a2f6cf5b819ee3d524a4a68ac
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-05 14:30:00 -0600

    created cta module

---
+++ src/components/CTA/index.tsx
@@ -0,0 +1,49 @@
+import React, { FC, JSX } from "react";
+import classes from "./CTA.module.scss";
+import Link from "next/link";
+
+interface Props {
+	backgroundColor: string;
+	title: {
+		title: JSX.Element;
+		color: string;
+	};
+	button: {
+		title: string;
+		link: string;
+		color: string;
+		backgroundColor: string;
+	};
+	isFloating?: boolean;
+}
+
+const CTA: FC<Props> = ({
+	title,
+	button,
+	isFloating = true,
+	backgroundColor,
+}) => {
+	return (
+		<aside
+			className={`${classes.cta} ${!isFloating ? classes.ctaFloating : ""}`}
+			style={{
+				backgroundColor,
+			}}
+		>
+			{title && <h3 style={{ color: title.color }}>{title.title}</h3>}
+			{button && button.title && (
+				<Link
+					href={button.link}
+					style={{
+						color: button.color,
+						backgroundColor: button.backgroundColor,
+					}}
+				>
+					{button.title}
+				</Link>
+			)}
+		</aside>
+	);
+};
+
+export default CTA;
