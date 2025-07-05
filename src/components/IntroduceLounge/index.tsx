ba9581b9654839702b2cb8fcc88b142d4067363a
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-05 14:52:03 -0600

    created introduce lounge module

---
+++ src/components/IntroduceLounge/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./IntroduceLounge.module.scss";
+
+const IntroduceLounge = () => {
+	return (
+		<section className={classes.introduce}>
+			<h3>Espacios versátiles que se adaptan a todo tipo de celebraciones.</h3>
+			<p>Conoce nuestros salones.</p>
+		</section>
+	);
+};
+
+export default IntroduceLounge;
