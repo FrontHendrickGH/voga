3c80e75abb46ce9ae6d5013fd7c336a1086d1a49
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-05 22:35:56 -0600

    added present lounge module

--- src/components/PresentLounge/index.tsx
+++ src/components/PresentLounge/index.tsx
@@ -1,20 +1,37 @@
+"use client";
 import React from "react";
 import classes from "./PresentLounge.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const PresentLounge = () => {
+	const images = [
+		"/voga-lounge-1.png",
+		"/jardines-lounge-1.png",
+		"/voga-lounge-1.png",
+		"/jardines-lounge-1.png",
+	];
+	const images2 = [
+		"/jardines-lounge-1.png",
+		"/voga-lounge-1.png",
+		"/jardines-lounge-1.png",
+		"/voga-lounge-1.png",
+	];
 	return (
 		<section className={classes.present}>
-			<article>
-				<div>
+			<article className={classes.container}>
+				<div className={classes.infoLounge}>
 					<Image
 						src={"/voga-lounge.svg"}
 						alt="logo voga venue"
 						width={145}
 						height={76}
 					/>
-					<h3>El salón de eventos más exclusivo de Tijuana</h3>
+					<h3>El salón de eventos más exclusivo de Tijuana.</h3>
 					<ul>
 						<li>Diseño moderno</li>
 						<li>Elegante</li>
@@ -25,9 +42,149 @@ const PresentLounge = () => {
 						Capacidad: <br />
 						<span>500 personas</span>
 					</p>
-					<div>
+					<div className={classes.links}>
 						<Link href={"#"}>Cotiza ahora</Link>
 						<Link href={"#"}>Ver salón</Link>
+					</div>
+				</div>
+				<div className={classes.swiperContainer}>
+					<Swiper
+						modules={[Navigation]}
+						navigation={{
+							nextEl: ".custom-next",
+							prevEl: ".custom-prev",
+						}}
+						loop={true}
+						spaceBetween={0}
+						slidesPerView={1}
+					>
+						{images.map((src, i) => (
+							<SwiperSlide key={i}>
+								<figure className={classes.image}>
+									<Image
+										src={src}
+										alt={`Slide ${i}`}
+										className="slide-image"
+										fill
+									/>
+								</figure>
+							</SwiperSlide>
+						))}
+					</Swiper>
+
+					{/* Custom arrows */}
+					<div className={classes.buttons}>
+						<button className="custom-prev">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="12"
+								height="23"
+								viewBox="0 0 12 23"
+								fill="none"
+							>
+								<path
+									d="M5.46392e-07 11.5L12 22.3253L12 0.674682L5.46392e-07 11.5Z"
+									fill="#449F82"
+								/>
+							</svg>
+						</button>
+						<button className="custom-next">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="12"
+								height="23"
+								viewBox="0 0 12 23"
+								fill="none"
+							>
+								<path
+									d="M12 11.5L-6.59078e-08 22.3253L8.80472e-07 0.674682L12 11.5Z"
+									fill="#449F82"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</article>
+			<article className={classes.container}>
+				<div className={classes.swiperContainer}>
+					<Swiper
+						modules={[Navigation]}
+						navigation={{
+							nextEl: ".custom-next-2",
+							prevEl: ".custom-prev-2",
+						}}
+						loop={true}
+						spaceBetween={0}
+						slidesPerView={1}
+					>
+						{images2.map((src, i) => (
+							<SwiperSlide key={i}>
+								<figure className={classes.image}>
+									<Image
+										src={src}
+										alt={`Slide ${i}`}
+										className="slide-image"
+										fill
+									/>
+								</figure>
+							</SwiperSlide>
+						))}
+					</Swiper>
+
+					{/* Custom arrows */}
+					<div className={classes.buttons}>
+						<button className="custom-prev-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="12"
+								height="23"
+								viewBox="0 0 12 23"
+								fill="none"
+							>
+								<path
+									d="M5.46392e-07 11.5L12 22.3253L12 0.674682L5.46392e-07 11.5Z"
+									fill="#449F82"
+								/>
+							</svg>
+						</button>
+						<button className="custom-next-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="12"
+								height="23"
+								viewBox="0 0 12 23"
+								fill="none"
+							>
+								<path
+									d="M12 11.5L-6.59078e-08 22.3253L8.80472e-07 0.674682L12 11.5Z"
+									fill="#449F82"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<div className={classes.infoLounge}>
+					<Image
+						src={"/jardines-lounge.svg"}
+						alt="logo voga venue"
+						width={145}
+						height={76}
+					/>
+					<h3>El jardín de eventos más versátil de Tijuana.</h3>
+					<ul>
+						<li>Jardín con un salon acogedor</li>
+						<li>Ideal para celebraciones íntimas</li>
+						<li>Ambiente versátil</li>
+					</ul>
+					<p style={{ color: "#9cb731" }}>
+						Capacidad: <br />
+						<span style={{ color: "#242C3F" }}>250 personas</span>
+					</p>
+					<div className={classes.links}>
+						<Link href={"#"}>Cotiza ahora</Link>
+						<Link href={"#"} style={{ backgroundColor: "#9CB731" }}>
+							Ver salón
+						</Link>
 					</div>
 				</div>
 			</article>
