37339d5bc19e15f03105124a5cd81b9537f42910
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-05 23:28:00 -0600

    fixing navbar at the top

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,70 +10,75 @@ const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<nav className={styles.nav}>
-			<figure className={styles.logo}>
-				<Image
-					src={"/voga-logo.svg"}
-					alt="voga logo"
-					fill
-					className={styles.image}
-				/>
-			</figure>
+		<>
+			<div role="figure" className={styles.gap} />
+			<div className={styles.container}>
+				<nav className={styles.nav}>
+					<figure className={styles.logo}>
+						<Image
+							src={"/voga-logo.svg"}
+							alt="voga logo"
+							fill
+							className={styles.image}
+						/>
+					</figure>
 
-			<button
-				className={styles.hamburger}
-				onClick={() => setIsOpen(!isOpen)}
-				aria-label="Toggle menu"
-			>
-				{isOpen ? <FiX /> : <FiMenu />}
-			</button>
+					<button
+						className={styles.hamburger}
+						onClick={() => setIsOpen(!isOpen)}
+						aria-label="Toggle menu"
+					>
+						{isOpen ? <FiX /> : <FiMenu />}
+					</button>
 
-			<ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
-				<li>
-					<Link href="/nosotros">Nosotros</Link>
-				</li>
-				<Menu as="li" className={styles.menu}>
-					<MenuButton className={styles.menuButton}>
-						Servicios
-						<figure className={styles.containerArrow}>
-							<Image src={"/arrow-down.svg"} alt="open menu" fill />
-							<Image src={"/arrow-down-gray.svg"} alt="open menu" fill />
-						</figure>
-					</MenuButton>
-					<MenuItems className={styles.menuItems}>
-						<MenuItem>
-							<Link href="#">Nuestra cocina</Link>
-						</MenuItem>
-						<MenuItem>
-							<Link href="#">Coordinación</Link>
-						</MenuItem>
-						<MenuItem>
-							<Link href="#">Eventos corporativos</Link>
-						</MenuItem>
-					</MenuItems>
-				</Menu>
-				<Menu as="li" className={styles.menu}>
-					<MenuButton className={styles.menuButton}>
-						Salones
-						<figure className={styles.containerArrow}>
-							<Image src={"/arrow-down.svg"} alt="open menu" fill />
-							<Image src={"/arrow-down-gray.svg"} alt="open menu" fill />
-						</figure>
-					</MenuButton>
-					<MenuItems className={styles.menuItems}>
-						<MenuItem>
-							<Link href="#">Salón voga</Link>
-						</MenuItem>
-						<MenuItem>
-							<Link href="#">Los jardínes</Link>
-						</MenuItem>
-					</MenuItems>
-				</Menu>
-				<li>
-					<Link href="/contacto">Contacto</Link>
-				</li>
-			</ul>
-		</nav>
+					<ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
+						<li>
+							<Link href="/nosotros">Nosotros</Link>
+						</li>
+						<Menu as="li" className={styles.menu}>
+							<MenuButton className={styles.menuButton}>
+								Servicios
+								<figure className={styles.containerArrow}>
+									<Image src={"/arrow-down.svg"} alt="open menu" fill />
+									<Image src={"/arrow-down-gray.svg"} alt="open menu" fill />
+								</figure>
+							</MenuButton>
+							<MenuItems className={styles.menuItems}>
+								<MenuItem>
+									<Link href="#">Nuestra cocina</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="#">Coordinación</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="#">Eventos corporativos</Link>
+								</MenuItem>
+							</MenuItems>
+						</Menu>
+						<Menu as="li" className={styles.menu}>
+							<MenuButton className={styles.menuButton}>
+								Salones
+								<figure className={styles.containerArrow}>
+									<Image src={"/arrow-down.svg"} alt="open menu" fill />
+									<Image src={"/arrow-down-gray.svg"} alt="open menu" fill />
+								</figure>
+							</MenuButton>
+							<MenuItems className={styles.menuItems}>
+								<MenuItem>
+									<Link href="#">Salón voga</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link href="#">Los jardínes</Link>
+								</MenuItem>
+							</MenuItems>
+						</Menu>
+						<li>
+							<Link href="/contacto">Contacto</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</>
 	);
 };
 
