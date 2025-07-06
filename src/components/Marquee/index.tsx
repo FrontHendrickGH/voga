be86bc20809c5df64f7c1535082210344997376e
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-05 23:22:03 -0600

    finished module

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import MarqueeC from "react-fast-marquee";
 import classes from "./MarqueeLounge.module.scss";
+import Link from "next/link";
 
 const Marquee = () => {
 	return (
@@ -14,6 +15,9 @@ const Marquee = () => {
 			>
 				<h1 className={classes.title}>celebra con nosotros &nbsp;</h1>
 			</MarqueeC>
+			<Link href={"#"} className={classes.link}>
+				Cotiza tu evento
+			</Link>
 		</div>
 	);
 };
