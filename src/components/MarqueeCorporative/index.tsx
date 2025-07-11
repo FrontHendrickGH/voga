9e334b55b6314175bf60df2d9ea5d0bf606c6d50
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-11 01:48:46 -0600

    Adds corporative events marquee
    Introduces a marquee component to display
    corporative events.
    
    The marquee uses react-fast-marquee for smooth
    scrolling and includes styling for the text and
    background.

---
+++ src/components/MarqueeCorporative/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import classes from "./MarqueeCorporative.module.scss";
+
+const MarqueeCorporative = () => {
+	return (
+		<aside className={classes.marquee}>
+			<Marquee
+				gradient={false}
+				speed={60}
+				direction="left"
+				pauseOnHover={false}
+			>
+				<h1 className={classes.title}>Eventos corporativos &nbsp;</h1>
+			</Marquee>
+		</aside>
+	);
+};
+
+export default MarqueeCorporative;
