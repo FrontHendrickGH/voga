9b20ebc2a22f663c02e10a729c4b654e33f2cf4a
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-11 01:39:36 -0600

    Adds corporative events page
    Implements the corporative events page with a banner, content sections, and a contact form.
    
    The page aims to provide information about corporative events and encourage users to contact the company.

---
+++ src/app/eventos-corporativos/page.tsx
@@ -0,0 +1,29 @@
+import Banner from "@/components/Banner";
+import BringPeople from "@/components/BringPeople";
+import Footer from "@/components/Footer";
+import PresentLounge from "@/components/PresentLounge";
+import React from "react";
+
+const CorporativeEvents = () => {
+	return (
+		<div>
+			<Banner
+				title="Eventos Corporativos"
+				image={{
+					src: "/banner-corporativos.jpg",
+					alt: "Eventos corporativos hero",
+				}}
+			/>
+			<BringPeople />
+			<PresentLounge />
+			<Footer
+				paddingTop={120}
+				button={{ title: "Contáctanos", href: "#" }}
+				title="Agenda una llamada con nuestros expertos."
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+			/>
+		</div>
+	);
+};
+
+export default CorporativeEvents;
