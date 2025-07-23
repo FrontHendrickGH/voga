15be0e69153421e39d1c17685d7c6cfd1e2a142b
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-23 02:02:32 -0600

    using components in our cuisine page

--- src/app/nuestra-cocina/page.tsx
+++ src/app/nuestra-cocina/page.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/Footer";
 import SurpriseGuests from "@/components/SurpriseGuests";
 import Banner from "@/components/Banner";
 import GalleryDishes from "@/components/GalleryDishes";
+import OurChef from "@/components/OurChef";
+import CTA from "@/components/CTA";
 
 const NuestraCocina = () => {
 	return (
@@ -14,10 +16,31 @@ const NuestraCocina = () => {
 			/>
 			<SurpriseGuests />
 			<GalleryDishes />
+			<OurChef />
+			<CTA
+				backgroundColor="#AC3964"
+				isFloating={false}
+				button={{
+					title: "Coordinación",
+					backgroundColor: "#EDECEB",
+					color: "#242C3F",
+					link: "#",
+				}}
+				title={{
+					title: (
+						<span>
+							Conoce a detalle nuestro
+							<br />
+							servicio de coordinación
+						</span>
+					),
+					color: "#EDECEB",
+				}}
+			/>
 			<Footer
-				paddingTop={120}
+				paddingTop={270}
 				button={{ title: "Contáctanos", href: "#" }}
-				title="Agenda una llamada con nuestros expertos."
+				title="Haz Realidad el Evento de Tus Sueños"
 				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
 			/>
 		</div>
