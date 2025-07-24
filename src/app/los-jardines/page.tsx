e0035719ab37db1d21dfffd024830a3ceefb6a6d
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-24 01:06:58 -0600

    added footer to each lounge

--- src/app/los-jardines/page.tsx
+++ src/app/los-jardines/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "../page.module.css";
 import IntroduceLounges from "@/components/IntroduceLounges";
+import Footer from "@/components/Footer";
 
 const LosJardines = () => {
 	return (
 		<div className={styles.page}>
 			<IntroduceLounges theme="Los-jardines" />
+			<Footer
+				paddingTop={216}
+				button={{ title: "Agenda una visita", href: "#" }}
+				title="¿Listo para planear el evento de tus sueños?"
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+			/>
 		</div>
 	);
 };
--- src/app/salon-voga/page.tsx
+++ src/app/salon-voga/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "../page.module.css";
 import IntroduceLounges from "@/components/IntroduceLounges";
+import Footer from "@/components/Footer";
 
 const SalonVoga = () => {
 	return (
 		<div className={styles.page}>
 			<IntroduceLounges theme="Voga" />
+			<Footer
+				paddingTop={216}
+				button={{ title: "Agenda una visita", href: "#" }}
+				title="¡Planeemos el evento de tus sueños juntos!"
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+			/>
 		</div>
 	);
 };
