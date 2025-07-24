6ef6d9733a95b33854e7b5cf2712cc2bc9af4539
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-24 17:11:03 -0600

    using component into new page

---
+++ src/app/coordinacion-y-decoracion/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
+import PerfectCoordination from "@/components/PerfectCoordination";
+
+const index = () => {
+	return (
+		<div>
+			<Banner
+				title="Coordinación y Decoración"
+				image={{
+					src: "/banner-coordinacion.jpg",
+					alt: "Coordinación y decoración hero",
+				}}
+			/>
+			<PerfectCoordination />
+			<Footer
+				paddingTop={120}
+				button={{ title: "Contáctanos", href: "#" }}
+				title="Planeemos tu evento juntos."
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+			/>
+		</div>
+	);
+};
+
+export default index;
