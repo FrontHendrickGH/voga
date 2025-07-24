bb18c3e843e04e5d649138d807d5cf7bd8b1566f
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-24 15:35:20 -0600

    creating special day module

---
+++ src/components/SpecialDay/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+import classes from "./SpecialDay.module.scss";
+import Link from "next/link";
+
+const SpecialDay = () => {
+	return (
+		<section className={classes.special}>
+			<Image
+				src={"/special-day-background.jpg"}
+				alt="background image decoration for your special day!"
+				fill
+			/>
+			<div className={classes.container}>
+				<div className={classes.containerTitle}>
+					<h2>
+						Un día especial,
+						<br />
+						merece un lugar
+					</h2>
+					<figure className={classes.image}>
+						<Image
+							fill
+							src={"/special-day-text.png"}
+							alt="special day text 'Especial'"
+						/>
+					</figure>
+					<p>
+						Queremos celebrar contigo los momentos más importantes <br /> de tu
+						vida ayudándote a que todo salga a la perfección.
+					</p>
+					<Link href={"#"}>Cotiza tu evento</Link>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SpecialDay;
