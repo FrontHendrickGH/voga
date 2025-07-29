22978bf2bad38151e5f5cfefb81884f1189a4363
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-28 19:07:01 -0600

    arranging contact page

---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "@/components/Footer";
+
+const Contacto = () => {
+	return (
+		<div>
+			<Footer
+				paddingTop={120}
+				button={{ title: "Contáctanos", href: "/contacto" }}
+				title="Planeemos tu evento juntos."
+				description="Contáctanos para agendar una visita y descubre cómo podemos hacer de tu evento una experiencia única."
+			/>
+		</div>
+	);
+};
+
+export default Contacto;
