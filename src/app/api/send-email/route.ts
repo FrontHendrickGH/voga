8de8a5245808fbe72ba1569938b26a8e7f16bf21
Hendrick <hendrickgalarzahernandez@gmail.com>
2025-07-29 22:05:32 -0600

    sent all data for backend

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -7,40 +7,43 @@ interface FormData {
 	month: string;
 	year: string;
 	name: string;
+	phone: string;
 	email: string;
 	message: string;
 }
 
 export async function POST(req: NextRequest) {
-	const body: FormData = await req.json();
-	const { event, numberOfGuests, day, month, year, name, email, message } =
-		body;
-	console.log({
-		event,
-		numberOfGuests,
-		day,
-		month,
-		year,
-		name,
-		email,
-		message,
-	});
-
-	return NextResponse.json({ success: true });
 	try {
+		const body: FormData = await req.json();
+
+		const {
+			event,
+			numberOfGuests,
+			day,
+			month,
+			year,
+			name,
+			email,
+			message,
+			phone,
+		} = body;
+
 		const result = await emailjs.send(
 			process.env.SERVICE_ID!,
 			process.env.TEMPLATE_ID!,
-			{ name, email, message, event, numberOfGuests, day, month, year },
+			{ name, email, message, event, numberOfGuests, day, month, year, phone },
 			{
 				publicKey: process.env.EMAIL_PUBLIC_KEY,
 				privateKey: process.env.EMAIL_KEY,
 			}
 		);
 
-		return res.status(200).json({ success: true, result });
+		return NextResponse.json({ success: true, result }, { status: 200 });
 	} catch (err) {
 		console.error(err);
-		return res.status(500).json({ error: "Failed to send email" });
+		return NextResponse.json(
+			{ error: "Failed to send email" },
+			{ status: 500 }
+		);
 	}
 }
